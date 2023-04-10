c2dcfaed655a6ff1edae0c278ba3a5f2575b056c
George Donnelly <me@georgedonnelly.com>
2023-04-10 14:22:09 -0500

    move colors into theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,18 +3,18 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      'kfblue': '#004D81',
+      'kfyellow': '#FFBC00',
+      'kfwhite': '#FFFDFD',
+      transparent: 'transparent',
+      primary: colors.kfblue,
     extend: {
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  colors: {
-    primary: colors.kfblue,
-    'kfblue': '#004D81',
-    'kfyellow': '#FFBC00',
-    'kfwhite': '#FFFDFD',
-    transparent: 'transparent',
   },
   plugins: [require("@tailwindcss/typography")],
 };
