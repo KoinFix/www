5ec9f781591273dd952ae99f605a5bc4bc59b195
George Donnelly <me@georgedonnelly.com>
2023-04-11 09:17:42 -0500

    move KF colors to extend section of tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,16 +5,15 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      'kfblue': '#004D81',
-      'kfyellow': '#FFBC00',
-      'kfwhite': '#FFFDFD',
-      transparent: 'transparent',
-      primary: colors.kfblue,
-    },
     extend: {
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'kfblue': '#004D81',
+        'kfyellow': '#FFBC00',
+        'kfwhite': '#FFFDFD',
+        transparent: 'transparent',
       },
     },
   },
